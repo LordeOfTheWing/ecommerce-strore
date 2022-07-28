07444ee1dd9dae34a5967e87b072e2ccb013d67a
Clinton Mainya <klyntonmainya@gmail.com>
2022-07-29 02:20:00 +0300

    Add image address locations

--- src/data.ts
+++ src/data.ts
@@ -1,21 +1,21 @@
 export const sliderItems = [
   {
     id: 1,
-    img: "https://i.ibb.co/XsdmR2c/1.png",
+    img: "https://o.remove.bg/downloads/92c2d045-462b-47de-bad5-dd1154b87728/pexels-juan-mendez-1536619-removebg-preview.png",
     title: "SUMMER SALE",
     desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
     bg: "f5fafd",
   },
   {
     id: 2,
-    img: "https://i.ibb.co/DG69bQ4/2.png",
+    img: "https://o.remove.bg/downloads/a76d3962-6b52-4000-a53e-aacebc97e215/pexels-destiawan-nur-agustra-3609061-removebg-preview.png",
     title: "AUTUMN COLLECTION",
     desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
     bg: "fcf1ed",
   },
   {
     id: 3,
-    img: "https://i.ibb.co/cXFnLLV/3.png",
+    img: "https://o.remove.bg/downloads/8296341e-2a83-4deb-a093-188295c847f8/pexels-photo-3049914-removebg-preview.png",
     title: "LOUNGEWEAR LOVE",
     desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
     bg: "fbf0f4",
